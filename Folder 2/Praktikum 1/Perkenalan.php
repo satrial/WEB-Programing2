d65163d619048abb3f219d29e3244ed67117748d
satrial <satriaindriawan@gmail.com>
2020-10-24 15:13:23 +0700

    first commit

---
+++ Folder 2/Praktikum 1/Perkenalan.php
@@ -0,0 +1,16 @@
+<?php
+class Perkenalan extends CI_Controller{
+    public function index() {
+        echo "<h1>Perkenalkan</h1>";
+
+    echo"<h2>Nama saya Satria
+
+        Saya tinggal di Kota Tegal
+
+
+        olah raga yang saya sukai adalah
+
+        Sepak Bola</h2>";
+    }
+
+}
